49a525e33699502ce3d1a8a0bf85972447153949
dobleduche <masebk1@gmail.com>
2025-11-11 03:16:03 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+export default {
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001'
+    }
+  }
+}
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
